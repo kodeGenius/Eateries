324bf497b1aec97769b7b7f23d613aa9b8ae0fc8
Saheed Oladosu <oladosu777@gmail.com>
2019-06-02 14:23:41 +0100

    created class and added the return html

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,6 +4,19 @@ import './SearchBar.css';
 
 class SearchBar extends React.Component{
     render(){
-        
+        <div class="SearchBar">
+            <div class="SearchBar-sort-options">
+                <ul>
+                <!-- Use .renderSortByOptions() to sort the businesses by their options -->
+                </ul>
+            </div>
+            <div class="SearchBar-fields">
+                <input placeholder="Search Businesses" />
+                <input placeholder="Where?" />
+            </div>
+            <div class="SearchBar-submit">
+                <a>Let's Go</a>
+            </div>
+        </div>
     }
 }
