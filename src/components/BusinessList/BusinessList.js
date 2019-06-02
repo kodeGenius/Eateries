dc4981aff73784dbc539e25fcaa6167881b38758
Saheed Oladosu <oladosu777@gmail.com>
2019-06-02 13:47:22 +0100

    added businesslist class

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,3 +2,17 @@ import React from 'react';
 import './Business.css';
 import Business from '../Business/Business';
 
+class BusinessList extends React.Component{
+    render(){
+        return(
+            <div class="BusinessList">
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+            </div>
+        )
+    }
+}
