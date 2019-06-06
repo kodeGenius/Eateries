a0873b1daa049c664fe93d206b1e5afd50ecc766
Saheed Oladosu <oladosu777@gmail.com>
2019-06-06 20:21:18 +0100

    new additions to search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './SearchBar.css';
 
+const sortByOptions = {
+    'Best Match': 'best_match',
+    'Highest Rated': 'rating',
+    'Most Viewed': 'review_count'
+}
 
 class SearchBar extends React.Component{
+    
+    renderSortByOptions(){
+        return Object.keys(sortByOptions).map(sortByOptions => {
+            let sortByOptionValue = sortByOptions[sortByOption];
+            return <li key="sortByOptionValue"></li>
+        });
+        
+    }
     render(){
         <div class="SearchBar">
             <div class="SearchBar-sort-options">
                 <ul>
-                <!-- Use .renderSortByOptions() to sort the businesses by their options -->
+                 <!-- Use .renderSortByOptions() to sort the businesses by their options -->
                 </ul>
             </div>
             <div class="SearchBar-fields">
