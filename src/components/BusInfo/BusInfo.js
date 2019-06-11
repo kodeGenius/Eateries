7dcdcd3b555f92341f406057f0077ed6c1ac11c7
Saheed Oladosu <oladosu777@gmail.com>
2019-06-11 14:32:55 +0100

    Bus

---
+++ src/components/BusInfo/BusInfo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './BusInfo.css';
+
+function BusInfo(){
+    return(
+        <div className = "foot">
+            <h3><span className="Who">Developed By:</span> Saheed Oladosu</h3>
+            <p>Contact information: <a href="mailto:oladosu777@gmail.com">
+  oladosu777@gmail.com</a>.</p>
+            <p>Twitter: <a href="@saheedolados">@saheedolados</a>.</p>
+        </div>
+    )
+}
+
+export default BusInfo;
