05ad1ee58d85425f57df1c84cd3837d95603fdc0
Saheed Oladosu <oladosu777@gmail.com>
2019-06-13 16:59:50 +0100

    props movement

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -3,38 +3,25 @@ import React from 'react';
 import './Business.css';
 import './pizza.jpg';
 
-
-const business = {
-    imageSrc: 'pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-}
-
 //business component
 class Business extends React.Component{
     render(){
         return(
             <div className="Business">
                 <div className="image-container">
-                    {<img src="{business.imageSrc}" alt="ima" />}
+                    <p>{this.props.business.imageSrc}</p>
                 </div>
-                <h2>{business.name}</h2>
+                <h2>{this.props.business.name}</h2>
                 <div className="Business-information">
                     <div className="Business-address">
-                        <p>{business.address}</p>
-                        <p>{business.city}</p>
-                        <p>{business.zipCode}</p>
+                        <p>{this.props.business.address}</p>
+                        <p>{this.props.business.city}</p>
+                        <p>{this.props.business.zipCode}</p>
                     </div>
                     <div className="Business-reviews">
-                        <h3>{business.category}</h3>
-                        <h3 className="rating">{business.rating}</h3>
-                        <p>{business.reviewCount}</p>
+                        <h3>{this.props.business.category}</h3>
+                        <h3 className="rating">{this.props.business.rating}</h3>
+                        <p>{this.props.business.reviewCount}</p>
                     </div>
                 </div>
             </div>
