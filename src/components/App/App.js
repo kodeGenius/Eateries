8d6e08e4921259fc6b2a3fecb7a6acf6c518cb68
Saheed Oladosu <oladosu777@gmail.com>
2019-06-13 15:47:24 +0100

    passing props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,10 +4,24 @@ import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 import BusInfo from '../BusInfo/BusInfo';
 
+const business = {
+  imageSrc: 'pizza.jpg',
+name: 'MarginOtto Pizzeria',
+address: '1010 Paddington Way',
+city: 'Flavortown',
+state: 'NY',
+zipCode: '10101',
+category: 'Italian',
+rating: 4.5,
+reviewCount: 90
+}
+
+const businesses = [business, business, business, business, business, business];
+
 function App() {
   return (
   <div className="App">
-    <p><img src="./ravenous_favicon.ico" alt="icon" /></p>
+    <p><img src="./ravenous_favicon.ico" className="logo" alt="icon" /></p>
     <h1>Eateries</h1>
     <SearchBar />
     <BusinessList />
