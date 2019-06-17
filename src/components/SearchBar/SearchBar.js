c4ecb0ee5bba27d9f7cd50d957f392ead1f15d9f
Saheed Oladosu <oladosu777@gmail.com>
2019-06-17 23:04:09 +0100

    new changes1

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -26,6 +26,10 @@ class SearchBar extends React.Component{
             return '';
         }
     }
+    handleSortByChange(sortByOption){
+        this.setState(
+            sortByOption: 'sortBy',
+        )}
     
     renderSortByOptions(){
         return Object.keys(sortByOptions).map(sortByOptions => {
