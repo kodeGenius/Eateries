feca536bcb5f3ea57c91a0ea12f56c3cdb07fa5f
Saheed Oladosu <oladosu777@gmail.com>
2019-06-17 18:11:19 +0100

    constructor added to component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -8,6 +8,10 @@ const sortByOptions = {
 }
 
 class SearchBar extends React.Component{
+    constructor(props){
+        super(props);
+        
+    }
     
     renderSortByOptions(){
         return Object.keys(sortByOptions).map(sortByOptions => {
