c504835ee9a090cd21c62e395642200e793f329b
Saheed Oladosu <oladosu777@gmail.com>
2019-06-19 17:54:48 +0100

    new changes

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -32,10 +32,10 @@ class SearchBar extends React.Component{
         });
     }
     handleTermChange(event){
-        this.setState({term: event.target.value})
+        this.setState({term: event.target.value});
     }
     handleLocationChange(event){
-        this.setState({})
+        this.setState({locattion: event.target.value});
     }
     
     renderSortByOptions(){
