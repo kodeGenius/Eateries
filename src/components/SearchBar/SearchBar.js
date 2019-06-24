0eab8d6e40fda09dcf55b0d74954688b6f67d3df
Saheed Oladosu <oladosu777@gmail.com>
2019-06-24 12:51:29 +0100

    new addition

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -39,7 +39,8 @@ class SearchBar extends React.Component{
     }
     
     handleSearch(event){
-        this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy)
+        this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy;
+        event.preventDefault();
     }
 
     renderSortByOptions(){
