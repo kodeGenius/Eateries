b111c00ea6aa784f090fb8e3adce7fda1a2ce00b
Saheed Oladosu <oladosu777@gmail.com>
2019-06-25 21:10:38 +0100

    add object to yelp file

--- src/util/yelp.js
+++ src/util/yelp.js
@@ -1,3 +1,5 @@
 const apikey ="YqRScCgClJAqWZWGIJCNrJWkN0Y6nqJ-nMnvxklAPY0DNiNGDHF5c7xWO4lDUMPBPa7BVf9gQjKGKXmbYyNpPoMON5kbcTFPJmH7zpdlvGHdD45_q3wV6obUYv4IXHYx";
 
-fetch
+const Yelp ={
+    search(term, location, sortBy){}
+}
