6c33e4bad44c5c800cd7884537a472670174411b
Saheed Oladosu <oladosu777@gmail.com>
2019-06-25 21:28:29 +0100

    added headers

--- src/util/yelp.js
+++ src/util/yelp.js
@@ -4,7 +4,9 @@ const Yelp ={
     search(term, location, sortBy){
         return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
         {
-            
+            headers:{
+                Authorization: `Bearer ${apikey}`
+            }
         }
         )
     }
