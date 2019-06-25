8cefc1e0ff5b2fc44d2154c12fb75f564b39469d
Saheed Oladosu <oladosu777@gmail.com>
2019-06-25 21:13:34 +0100

    added fetch

--- src/util/yelp.js
+++ src/util/yelp.js
@@ -1,5 +1,7 @@
 const apikey ="YqRScCgClJAqWZWGIJCNrJWkN0Y6nqJ-nMnvxklAPY0DNiNGDHF5c7xWO4lDUMPBPa7BVf9gQjKGKXmbYyNpPoMON5kbcTFPJmH7zpdlvGHdD45_q3wV6obUYv4IXHYx";
 
 const Yelp ={
-    search(term, location, sortBy){}
+    search(term, location, sortBy){
+        return fetch()
+    }
 }
