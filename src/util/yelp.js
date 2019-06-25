01e15279288aae22d30d625ac8217152ad559eac
Saheed Oladosu <oladosu777@gmail.com>
2019-06-25 21:27:03 +0100

    added ojb

--- src/util/yelp.js
+++ src/util/yelp.js
@@ -2,7 +2,10 @@ const apikey ="YqRScCgClJAqWZWGIJCNrJWkN0Y6nqJ-nMnvxklAPY0DNiNGDHF5c7xWO4lDUMPBP
 
 const Yelp ={
     search(term, location, sortBy){
-        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+        {
+            
+        }
         )
     }
 }
