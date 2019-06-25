b0c2ca3002bf6c5a74a5c9a89529fcd69e4642d9
Saheed Oladosu <oladosu777@gmail.com>
2019-06-25 21:19:38 +0100

    added cors anywhere key

--- src/util/yelp.js
+++ src/util/yelp.js
@@ -2,6 +2,7 @@ const apikey ="YqRScCgClJAqWZWGIJCNrJWkN0Y6nqJ-nMnvxklAPY0DNiNGDHF5c7xWO4lDUMPBP
 
 const Yelp ={
     search(term, location, sortBy){
-        return fetch()
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`
+        )
     }
 }
